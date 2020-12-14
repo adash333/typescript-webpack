044f802de7a84dcb7234d364a5bb08bf8cc81b46
adash333 <adash333@hotmail.com>
2020-12-14 22:04:19 +0900

    uuid

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
+import { v4 } from 'uuid';
 import { User } from "./User";
 
-const user = new User('佐藤', '次郎', 31);
+const user = new User('山田', '花子', 28);
 
 const contentsElem = document.getElementById('contents');
 if(!!contentsElem) {
     contentsElem.innerText = `${user.familyName} ${user.givenName}`;
+}
+
+const saibanButton = document.getElementById('saiban') as HTMLButtonElement;
+const uuidSpan = document.getElementById('uuid') as HTMLSpanElement;
+
+// ボタンのクリックイベント
+saibanButton.onclick = (e) => {
+    uuidSpan.innerText = v4();
 }
